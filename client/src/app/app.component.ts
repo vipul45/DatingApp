c8109a0c6a60096db09c5504412857fdee76bc4f
vipul45 <84368833+vipul45@users.noreply.github.com>
2024-01-26 00:04:50 +0530

    Commit 2

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'Find One';
+  users : any;
+  constructor(private http: HttpClient){}
+  ngOnInit(){
+    this.getUsers();
+  }
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+}
